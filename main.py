9fd144ca81e0a2d42433330468399988bc5dcd4f
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-21 16:30:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-import sys
-try:prg = sys.argv[1]
-except: prg = input('Enter program name: ')
+import sys,argparse
+argparser = argparse.ArgumentParser(prog = '',description = 'a simple programing language')
+argparser.add_argument('file')
+argparser.add_argument('-d', '--debug',action='store_true')  
+argparser.add_argument('--allow_inf_loop',action='store_true')  
+argparser.add_argument('-l', '--loop_limit',default=1000000,type=int)      
+cli_args = argparser.parse_args()
 
-def stop(msg,tkn="unspecified"):
-  print(msg,'\n'+'token',tkn)
+debug = cli_args.debug
+lpbrk = not (cli_args.allow_inf_loop)
+if lpbrk:
+  LOOPLIMIT = cli_args.loop_limit
+else: LOOPLIMIT = 0;
+
+def stop(msg):
+  print(msg)
   sys.exit()
 
-with open(f'{prg}', 'r') as f:
+with open(f'{cli_args.file}', 'r') as f:
   c = f.read()
-
+  
 vars_ = {}
+def fmtstr(strn):
+  curstr = ""
+  ad = False
+  for chr in strn:
+    if chr == "{":
+      ad=True
+    elif chr == "}":
+      ad = False
+      rs = "{%s}"%curstr
+      try:
+        strn = strn.replace(rs,str(vars_[curstr.replace("$","")]))
+      except KeyError:
+        stop("Error: %s is not a valid variable name"%curstr)
+      curstr = ""
+    elif ad:
+      curstr+=chr
+  return strn
 fns = {}
 PI = 3.14159
 E = 2.71828
@@ -22,6 +49,9 @@ def run(txt):
   tkno = 0
   for tk in tokens:
     tkno+=1
+    if debug:
+      print(f"\n{tkno}: "+tk,flush=True)
+      print(vars_)
     tk2 = tk.split(':',1)
     if tk2[0].startswith('import'):
       p_n = tk2[0][7:]
@@ -30,7 +60,12 @@ def run(txt):
           p = fil.read()
           run(p)
       except FileNotFoundError:
-        stop(f"File {p_n}.ok not found",tkno)
+        try:
+          with open(f"pkgs/{p_n}.ok",'r') as fil:
+            p = fil.read()
+            run(p)
+        except:    
+          stop(f"no module {p_n}")
     elif tk2[0].startswith('call'):
       p_n = tk2[0].split(' ')[1]
     
@@ -39,15 +74,25 @@ def run(txt):
           p = fil.read()
           run(p)
       except FileNotFoundError:
-        stop(f"File {p_n}.ok not found",tkno)
+        stop(f"File {p_n}.ok not found")
     elif tk2[0] == ('set'):
       tk3 = tk2[1].split('=')
+      
       if tk3[1][0] == "i":
         vars_[tk3[0]] = int(tk3[1][1:])
       elif  tk3[1][0]== "f":
         vars_[tk3[0]] = float(tk3[1][1:])
-      elif tk3[1][0]== "'": vars_[tk3[0]] = tk3[1][1:][:-1]
-      elif tk3[1][0]== "\"": vars_[tk3[0]] = tk3[1][1:][:-1]
+  
+      elif tk3[1][1]== "'":
+        if tk3[1][0] == "F":
+          vars_[tk3[0]] = fmtstr(tk3[1][2:-1])
+        elif tk3[1][0] == "R":
+          vars_[tk3[0]] = tk3[1][2:-1]
+      elif tk3[1][1]== "\"": 
+        if tk3[1][0] == "F":
+          vars_[tk3[0]] = fmtstr(tk3[1][2:-1])
+        elif tk3[1][0] == "R":
+          vars_[tk3[0]] = tk3[1][2:-1]
       elif tk3[1][0]== "(":
         s = (tk3[1])[1:][:-1]
         for ke,va in vars_.items():
@@ -64,7 +109,7 @@ def run(txt):
           pass
         vars_[tk3[0]] = val
       else:
-        stop("expected type idenifier",tkno)
+        stop("expected type idenifier")
      
     elif tk2[0] == "print":
       if tk2[1][0] == "&":
@@ -74,7 +119,7 @@ def run(txt):
         try:
           print(vars_[tk2[1][1:]],end='')
         except KeyError:
-          stop("variable not found "+tk2[1][1:],tkno)
+          stop("variable not found "+tk2[1][1:])
       else:
         print(tk2[1],end='')
           
@@ -84,8 +129,8 @@ def run(txt):
       for ke,va in vars_.items():
           exp = exp.replace(f"${ke}",str(va))
       try: eval(exp)
-      except SyntaxError:
-        stop("variable not found",tkno)
+      except:
+        stop("variable not found")  
       if (eval(exp)):
         cd = tk2[1][1:][:-1].split('|')
         for c in cd:
@@ -95,6 +140,7 @@ def run(txt):
       for ke,va in vars_.items():
           exp = exp.replace(f"${ke}",str(va))
       co = tk2[1][1:][:-1].split('|')
+      lpcnt = 0
       while True:
         if (eval(exp)):
           for c2 in co:
@@ -102,6 +148,8 @@ def run(txt):
           exp = tk2[0][1:][:-1]
           for ke,va in vars_.items():
             exp = exp.replace(f"${ke}",str(va))
+          lpcnt += 1 
+          if lpcnt>=LOOPLIMIT and lpbrk:stop("loop limit exceded")
         else:break
     elif tk2[0][0] == "%":
       nm = tk2[0][1:]
