696694cde0af1177b0861359dfa3c90fa35884b3
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-25 13:14:08 -0500

    Update parse_var.py

--- parse_var.py
+++ parse_var.py
@@ -1,12 +1,18 @@
 import parse_func
 def parse_var(val):
   if val.startswith("'") or val.startswith('"'):
-        val = val[1:][:-1]
         return val
+  elif val[0] == "[":
+    
+    return f"#{val}"
+  elif val.startswith("func"):
+     return (parse_func.parse_func_def(val)) 
+  elif val.startswith("evalpy"):
+        val = val.replace("evalpy(","")[:-2][1:]
+        return eval(val)
   elif '.' in val:
         return float(val)
-  elif val.startswith("func"):
-     return (parse_func.parse_func_def(val))
+  
   else:
         return int(val)
   
