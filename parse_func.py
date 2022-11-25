fbc9b62def63aeb6678a334f45df9947db897243
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-25 13:13:49 -0500

    Update parse_func.py

--- parse_func.py
+++ parse_func.py
@@ -1,6 +1,6 @@
 def parse_func_def(c):
+  
   c = c.replace("func","")
   c_args =  c[c.index("(") + 1 : c.index(")")].split(",")
   c_code = c[c.index("{") + 1 : c.index("}")]
-  print(c_code)
   return {"args":c_args,"code":c_code}
