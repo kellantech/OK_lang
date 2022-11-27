7a958e577c640c05dbd527145f916938dd4aa9c5
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-27 18:29:54 -0500

    Update parse_if.py

--- parse_if.py
+++ parse_if.py
@@ -3,7 +3,5 @@ def parse(st):
   st = st[3:].strip()
   cond = st[st.index("(") + 1 : st.index(")")]
   stat = st[st.index("{")+1:-1]
-  if cond_eval.eval_cond(cond) == True:
-    return stat
-  else:
-    return ""
+  return stat,cond
+  
