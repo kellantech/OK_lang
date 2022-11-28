c18ea3e8e079d0ea97b8b696b1447b4284539c65
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-28 17:54:02 -0500

    Update parse_var.py

--- parse_var.py
+++ parse_var.py
@@ -9,7 +9,11 @@ def parse_var(val):
      return (parse_func.parse_func_def(val)) 
   elif val.startswith("evalpy"):
         val = val.replace("evalpy(","")[:-2][1:]
-        return eval(val)
+        evv = eval(val)
+        if evv is int:
+          return evv
+        else:  
+          return f'"{evv}"'
   elif '.' in val:
         return float(val)
   
