b575cdba79552875e1e5497b791088b230229800
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-28 17:51:19 -0500

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -3,10 +3,10 @@ def load_args():
   argparser = argparse.ArgumentParser(prog = '',description = 'a simple programing language')
   argparser.add_argument('file')
   argparser.add_argument('-d', '--debug',action='store_true')  
-  argparser.add_argument('--allow_inf_loop',action='store_true')  
+  argparser.add_argument('-i','--inf',action='store_true')  
   argparser.add_argument('-l', '--loop_limit',default=1000000,type=int)      
   cli_args = argparser.parse_args()
-  lpbrk = not (cli_args.allow_inf_loop)
+  lpbrk = not (cli_args.inf)
   if lpbrk:
     LOOPLIMIT = cli_args.loop_limit
   
